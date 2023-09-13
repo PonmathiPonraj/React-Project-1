c21ea10cb1cbad9e12f41fe6b4497f01e6d60415
ponmathi <ponmathittl@gmail.com>
2023-09-13 12:19:29 +0530

    final commit

---
+++ src/Components/ProductForm.js
@@ -0,0 +1,71 @@
+import React, { useState } from 'react';
+import Card from './Card/Card';
+import classes from './ProductForm.module.css';
+
+const ProductForm = (props) => {
+  const [productId, setProductId] = useState('');
+  const [productName, setProductName] = useState('');
+  const [category, setCategory] = useState('');
+  const [price, setPrice] = useState('');
+  
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const formData = { productId, productName, category, price };
+    props.addProduct(formData);
+    setProductId('');
+    setProductName('');
+    setCategory('');
+    setPrice('');
+  };
+
+  const idHandler=(event)=>{
+    setProductId(event.target.value)
+  }
+
+  const productHandler=(event)=>{
+    setProductName(event.target.value)
+  }
+
+  const categoryHandler =(event)=>{
+    setCategory(event.target.value)
+  }
+
+  const priceHandler=(event)=>{
+    setPrice(event.target.value)
+  }
+  
+  return (
+    <Card>
+      <form  className={classes.form}onSubmit={handleSubmit}>
+        <label>
+          Product ID:
+          <input type="text" value={productId} onChange={idHandler} required />
+        </label>
+        <br />
+        <label>
+          Product Name:
+          <input type="text" value={productName} onChange={productHandler} required />
+        </label>
+        <br />
+        <label>
+          Category:
+          <select value={category} onChange={categoryHandler} required>
+            <option value="">Select a category</option>
+            <option value="food item">Food Item</option>
+            <option value="electronic item">Electronic Item</option>
+            <option value="skincare item">Skincare Item</option>
+          </select>
+        </label>
+        <br />
+        <label>
+          Price:
+          <input type="number" value={price} onChange={priceHandler} required />
+        </label>
+        <br />
+        <button type="submit">Add Item</button>
+      </form>
+    </Card>
+  );
+};
+  
+export default ProductForm;
---
+++ src/Components/ProductList.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import ElectronicItem from './ElectronicItem';
+import SkinCareItem from './SkinCareItem';
+import FoodItem from './FoodItem';
+
+const ProductList = (props) => {
+  const groupedProducts = {};
+
+  for (const product of props.products) {
+    const  category  = product.category;
+    if (!groupedProducts[category]) {
+      groupedProducts[category] = [product];
+    } else {
+      groupedProducts[category].push(product);
+    }
+  }
+
+  return (
+    <div>
+      <h2>Product List</h2>
+      <h2>Electronic Items</h2>
+      {groupedProducts['electronic item'] && <ElectronicItem products={groupedProducts['electronic items']} />}
+      <h2>SkinCare Items</h2>
+      {groupedProducts['skinCare item'] && <SkinCareItem  products={groupedProducts['skincare items']} />}
+      <h2>Food Items</h2>
+      {groupedProducts['food item'] && <FoodItem   products={groupedProducts['food item']}/>}
+    </div>
+  );
+};
+
+export default ProductList;
